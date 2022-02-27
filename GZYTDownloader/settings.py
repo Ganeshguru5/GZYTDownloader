7329b3123f56644a30f9534e17bc41bae2a35db5
Ganeshguru <61460873+Ganeshguru5@users.noreply.github.com>
2022-02-27 12:22:01 +0530

    Update settings.py

--- GZYTDownloader/settings.py
+++ GZYTDownloader/settings.py
@@ -18,6 +18,7 @@ import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+ALLOWED_HOSTS = ['127.0.0.1:8000','gzytdownloader.herokuapp.com']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
